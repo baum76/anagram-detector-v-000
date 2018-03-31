05c7a9f725a537d98313b122129c8d2facfab1e9
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-31 15:47:50 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,11 +3,12 @@ require "pry"
 class Anagram
 attr_accessor :word
 
-def initialize(word)
-  @word = word
-end
+  def initialize(word)
+    @word = word
+  end
 
-def match(anagram)
-  self.split(//).permutation.map {|a| a.join}
-binding.pry
-end
+  def match(anagram)
+    self.split(//).permutation.map {|a| a.join}
+    binding.pry
+  end
+end  
