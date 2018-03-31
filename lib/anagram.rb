102990dcdb1de3388fe4d33a1f6bebb1418c04cf
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-31 16:08:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ attr_accessor :word_to_analyze
     puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
     find_anagram_in_this_string.find_all do |word|
       if word.split("").sort == self.word_to_analyze.split("").sort
-        word
+        word_to_analyze
       end
     end
   end
