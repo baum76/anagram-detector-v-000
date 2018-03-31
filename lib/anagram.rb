fb7646968def556155d13e073f82ea1a13528371
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-31 15:52:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,12 @@ attr_accessor :word
     @word = word
   end
 
-  def match(anagram)
-    word_array.select do |word|
-      word.split("").sort == @word.split("").sort
-    end  
+  def match(find_anagram_in_this_string)
+    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    find_anagram_in_this_string.find_all do |word|
+      if word.split("").sort == self.word_to_analyze.split("").sort
+        word
+      end
+    end
   end
 end
