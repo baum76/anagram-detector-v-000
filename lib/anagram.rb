32f83de8e0c5ede389c89522da5f00aa22532dc0
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-31 15:56:17 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,4 +9,6 @@ attr_accessor :word_to_analyze
 
   def match(find_anagram_in_this_string)
     puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+  end
+    
 end
