4fa0a41a6497f34abc074bbab0d697935fe69d90
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-31 15:54:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,10 @@
 require "pry"
 
 class Anagram
-attr_accessor :word
+attr_accessor :word_to_analyze
 
   def initialize(word)
-    @word = word
+    @word_to_analyze = word_to_analyze
   end
 
   def match(anagram)
